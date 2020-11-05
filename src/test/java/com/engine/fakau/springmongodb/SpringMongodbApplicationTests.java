da484ff2aabe8b5fe0425b3911e2b94a677df5ad
Laurent Kafka M <laurentkafka123@gmail.com>
2020-11-05 10:47:55 -0500

    init

---
+++ src/test/java/com/engine/fakau/springmongodb/SpringMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.engine.fakau.springmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
